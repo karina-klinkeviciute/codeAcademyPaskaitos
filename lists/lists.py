22215eebaf438db649596d5d5a588568e5e396c6
karina <karina.klinkeviciute@.>
2023-04-19 18:52:36 +0300

    pridėjau lists.py

---
+++ lists/lists.py
@@ -0,0 +1,53 @@
+# Sąrašai
+
+eilute = "Labas rytas. Šiandien graži diena. Oras labai geras."
+
+print(eilute.split())
+
+sarasas = ["a", 15, "Labas rytas", ]
+
+
+raide = sarasas[2][6]
+
+print("raide: ", raide)
+
+tekstas = "Labas rytas"
+
+print("raide is sakinio: ", tekstas[2])
+
+print("sarasas: ", sarasas[2])
+
+zodziai = ["vienas", "du", "trys"]
+
+pirmas = sarasas[0]
+
+trecias = sarasas[2]
+
+print(pirmas)
+
+print(trecias)
+
+visko_po_truputi = [5, 5.6, "Lietuva", [5, 6, 15, ["labas", "rytas"]], True]
+
+vidinis_sarasas = visko_po_truputi[3]
+
+print(vidinis_sarasas)
+
+trecias_vidiniam = vidinis_sarasas[2]
+
+print(trecias_vidiniam)
+
+trecias_vidiniam2 = visko_po_truputi[3][3][1]
+
+print(trecias_vidiniam2)
+
+eilute = "zodis yra grazus"
+
+zodziu_sarasas = ["zodis", "yra", "grazus"]
+
+raide = eilute[15]
+
+zodziai = eilute.split()
+
+print(zodziu_sarasas)
+
