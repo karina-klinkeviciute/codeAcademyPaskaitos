4ab7133939b45685e26520392cf192dc3bc2e692
Karina <karina.klinkeviciute@gmail.com>
2023-04-19 18:49:58 +0300

    added dictionaries

---
+++ dictionaries/dictionaries1.py
@@ -0,0 +1,68 @@
+from pprint import pprint
+
+amzius = {"Rokas": 25, "Andrius": 45, "Ieva": 22}
+
+
+
+# ivestas = input("iveskite")
+
+# if ivestas.capitalize() in amzius:
+#     print("yra")
+# else:
+#     print("nera")
+
+amzius1 = amzius["Rokas"]
+
+print(amzius1)
+
+amzius["Tomas"] = 19
+
+print(amzius["Tomas"])
+
+amzius["Rokas"] = 32
+
+print(amzius["Rokas"])
+
+print(amzius)
+
+del amzius["Rokas"]
+
+print(amzius)
+
+a = amzius.pop("Andrius")
+
+print(amzius)
+
+print(a)
+
+zmogus1 = {
+    "vardas": "Jonas",
+    "darbas": "programuotojas",
+    "miestas": "Šiauliu",
+    "telefonas": "+370 607 12345",
+    "amžius": 26
+}
+
+zmogus2 = {
+    "vardas": "Agnė",
+    "darbas": "autobuso vairuotoja",
+    "miestas": "Kaunas",
+    "telefonas": "123",
+    "amžius": 26
+}
+
+zmogus3 = {
+    "vardas": "Tomas",
+    "darbas": "pardavėjas",
+    "miestas": "Kaunas",
+    "telefonas": "55555",
+    "amžius": 35
+}
+
+zmones = [zmogus1, zmogus2, zmogus3]
+
+pprint(zmones)
+
+import datetime
+
+datetime.date()
